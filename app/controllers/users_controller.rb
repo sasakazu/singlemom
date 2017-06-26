ff9a8b8cd226de3260e50147c2d2b5f838ee6973
sasakazu <dropout.college2014@gmail.com>
2017-06-26 12:29:11 +0900

    user show edit done......

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
+    @mom = Mom.find(params[:id])
   end
 
   def edit
   end
+
+
+
+
+
+  private
+
+      def user_params
+        params.require(:user).permit(:name, :age, :live, :children, :profile, :wish, :image)
+      end
+
+      def mom_params
+        params.require(:mom).permit(:name, :age, :live, :children, :profile, :wish, :image)
+      end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'pages/contact'
+  get 'pages/about'
 
 
   resources :users
