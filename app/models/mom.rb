04c0bb0d2a2d05e5b046663e65f59b620faacd9b
sasakazu <dropout.college2014@gmail.com>
2017-06-28 19:27:12 +0900

    background color layout done...........

--- app/models/mom.rb
+++ app/models/mom.rb
@@ -1,8 +1,9 @@
 class Mom < ApplicationRecord
   mount_uploader :image, ImageUploader
 
-  belongs_to :user, dependent: :destroy
+  # belongs_to :user, dependent: :destroy
 
+  belongs_to :user, optional: true, dependent: :destroy
 
 
 end
